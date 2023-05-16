b853cc804445fb7adfa2e3a2e8306e3c875d5cc0
Ben Fielder <bencfielder@gmail.com>
2023-05-16 16:22:36 +0100

    updates

--- src/spaceworm/pyramidnumbers.js
+++ src/spaceworm/pyramidnumbers.js
@@ -2,7 +2,6 @@ import React from "react"
 import {Link} from "react-router-dom";
 
 import logo3 from "../pictures/logo3.png"
-import pyramidnumbersapp from "../downloadables/Pyramid_Numbers_42.apk"
 
 export default class PyramidNumbers extends React.Component{
     render(){
@@ -12,7 +11,7 @@ export default class PyramidNumbers extends React.Component{
 		<a class="logo" href="#home">
               <img src={logo3} alt="logo" />
 		</a>
-		<nav>	
+		<nav>
 		 <ul class="nav-bar"><div class="bg"></div>
 			<li><Link class="nav-link" to="/">Home</Link></li>
 			<li><Link class="nav-link" to="/projects">Projects</Link></li>
@@ -26,8 +25,9 @@ export default class PyramidNumbers extends React.Component{
 		        <section class="intro">
 		            <h3>Pyramid Numbers</h3>
 		                <p>A puzzle game involving numbers and pyramids</p>
-		                <p>Download for Android on the <a href="https://play.google.com/store/apps/details?id=com.SpaceWorm.PyramidNumbers" target="_blank">Play Store</a>.</p>
-						<p>Or click <a href={pyramidnumbersapp} download="PyramidNumbers.apk">here</a>.</p>
+		                <p>Download for Android on the Play Store.</p>
+						<br></br>
+						<a href="https://play.google.com/store/apps/details?id=com.SpaceWorm.PyramidNumbers1" target="_blank"><i class="fab fa-google-play"></i> Pyramid Numbers</a>
                 
 
 		        </section> 

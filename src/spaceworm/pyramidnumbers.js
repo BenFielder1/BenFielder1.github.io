fdc764bb395eebbdd9478871eee433eef8c8f597
Ben Fielder <bencfielder@gmail.com>
2024-10-28 12:07:37 +0000

    updating Pyramid Numbers page

--- src/spaceworm/pyramidnumbers.js
+++ src/spaceworm/pyramidnumbers.js
@@ -14,9 +14,9 @@ export default class PyramidNumbers extends React.Component{
 		        <div id="pyramidnum"></div>
 		        <section class="intro">
 		            <h3>Pyramid Numbers</h3>
-		                <p>A puzzle game involving numbers and pyramids</p>
-		                <p>Download for Android by clicking the download button below</p>
-                        <p>Currently unavailable on the Play Store.</p>
+		                <p>A puzzle game involving numbers and pyramids.</p>
+		                <p>Download for Android by clicking the download button below.</p>
+                        <p>Currently untaking closed testing for the Play Store, so will be available there soon.</p>
 						<br></br>
 
                         <a href={pyramidnumbersapp} download="PyramidNumbers.apk"><i class="fa-solid fa-download"></i></a>
